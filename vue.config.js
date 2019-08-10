4cf95f4ad8ecaa5fb57dc5cf1820e7eaf5f454f7
Innei <tukon479@gmail.com>
2019-08-10 14:23:48 +0800

    add vue-cli config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,4 @@
 module.exports = {
     //publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
-    outputDir: __dirname + '../site/public'
+    outputDir: __dirname + '/../site/public'
 }
